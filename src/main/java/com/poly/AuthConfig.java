c63fdb733c81e1289fced5a2ea5485b6cbad63ed
Team6 <dothanhven0303@gmail.com>
2023-07-16 20:23:07 +0700

    UPDATE

---
+++ src/main/java/com/poly/AuthConfig.java
@@ -0,0 +1,85 @@
+package com.poly;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.endpoint.ReactiveOAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+
+import com.poly.Service.UserDetailsServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AuthConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	BCryptPasswordEncoder pe;
+	@Autowired
+    private UserDetailsServiceImpl userDetailsService;
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	// Quản lý dữ liệu người dùng
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
+
+	// Phân quyền sử dụng và hình thức đăng nhập
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// CSRF, CORS
+		http.csrf().disable().cors().disable();
+
+		// Phân quyền sử dụng
+//		http.authorizeRequests().antMatchers("/home/index", "/auth/login/**", "/login/oauth2/code/**","/account/**").permitAll()
+//				.anyRequest().authenticated();
+		http.authorizeRequests().anyRequest().permitAll();
+//		Điều khiển lỗi truy cập không đúng vai trò
+		http.exceptionHandling()
+		.accessDeniedPage("/auth/access/denied");
+		// Giao diện đăng nhập
+		http.formLogin().loginPage("/auth/login/form").loginProcessingUrl("/auth/login")// ACTION
+				.defaultSuccessUrl("/auth/login/success", false).failureUrl("/auth/login/error")
+				.usernameParameter("username").passwordParameter("password");
+
+		http.rememberMe().rememberMeParameter("remember");
+
+		// Đăng xuất
+		http.logout().logoutUrl("/auth/logoff").logoutSuccessUrl("/auth/logoff/success");
+
+		// Oauth2 - Đăng nhập từ mạng xã hội
+		http.oauth2Login().loginPage("/auth/login/form").defaultSuccessUrl("/oauth2/login/success", true)
+				.failureUrl("/oauth2/login/error").authorizationEndpoint().baseUri("/oauth2/authorization")
+				.authorizationRequestRepository(getRepository()).and().tokenEndpoint()
+				.accessTokenResponseClient(getToken());
+		http.httpBasic();
+	}
+
+	@Bean
+	public AuthorizationRequestRepository<OAuth2AuthorizationRequest> getRepository() {
+		return new HttpSessionOAuth2AuthorizationRequestRepository();
+	}
+
+	@Bean
+	public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> getToken() {
+		return new DefaultAuthorizationCodeTokenResponseClient();
+	}
+}
---
+++ src/main/java/com/poly/Bean/Account.java
@@ -0,0 +1,16 @@
+package com.poly.Bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+	String username;
+	String password;
+	String fullname;
+	String email;
+	String[] role;
+}
