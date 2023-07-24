bb351660b54787a5aef6c9a3e5795c92eb13594a
Team6 <dothanhven0303@gmail.com>
2023-07-24 16:54:56 +0700

    Ven UPDATe

--- src/main/java/com/poly/Controller/OrderController.java
+++ src/main/java/com/poly/Controller/OrderController.java
@@ -12,26 +12,37 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.poly.Bean.Order;
+import com.poly.Bean.OrderMap;
+import com.poly.DAO.OrderDAO;
+
 @Controller
 public class OrderController {
+	@Autowired
+	OrderDAO dao;
+
 	@RequestMapping("/admin/orders")
-	public String HomeOrder() {
-		
+	public String HomeOrder(Model model) {
+		OrderMap orders = dao.findAll();
+		model.addAttribute("rooms", orders);
 		return "admin/order";
 	}
-	
+
 	@RequestMapping("/admin/orders/detail-room/{id}")
-	public String showModal(@PathVariable("id") int id, Model model) {
-		model.addAttribute("id", id);
+	public String showModal(@PathVariable("id") String id, Model model) {
+		Order order = dao.findByKey(id);
+		model.addAttribute("room", order);
 		return "admin/modal-detail-room";
 	}
+
 	@Autowired
 	HttpServletRequest request;
+
 	@RequestMapping("/update/detail-room-serivce/{id}")
 	public String updateService(@PathVariable("id") int id, Model model) {
 		model.addAttribute("id", id);
 		HttpSession session = request.getSession();
-		session.setAttribute("message", "Sửa thành công phòng "+id);
+		session.setAttribute("message", "Sửa thành công phòng " + id);
 		return "redirect:/admin/orders";
 	}
 }
